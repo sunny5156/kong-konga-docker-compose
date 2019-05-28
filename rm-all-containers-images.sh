4744429f2fb5f607bbf93456b18f0c1d978cd65d
sunny5156 <sunny5156@qq.com>
2019-05-28 17:19:38 +0800

    删除容器 和 镜像

--- rm-all-containers-images.sh
+++ rm-all-containers-images.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/bash
 
-docker rm $(docker ps -a -q)
-docker rmi $(docker images -q) -f
+#docker rm $(docker ps -a -q)
+#docker rmi $(docker images -q) -f
+
+docker rm konga kong konga-database kong-database konga kong-database-migrations
+docker rmi kong-konga-docker-compose_konga:latest kong-konga-docker-compose_kong:latest kong-konga-docker-compose_kong-database-migrations:latest kong-konga-docker-compose_kong-database:latest kong-konga-docker-compose_konga-database:latest
